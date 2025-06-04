da67734482b8d7943328989272b5d0a52cbe4a1d
Zach Strout <43109787+RTnhN@users.noreply.github.com>
2025-06-03 22:47:12 -0500

    Fix: Make order of bday correct and fix format

--- PhotoTimeSleuth/Helpers/file_helper.py
+++ PhotoTimeSleuth/Helpers/file_helper.py
@@ -20,18 +20,18 @@ def load_names_and_bdays(bday_file):
                     "It looks like the bday file is not formatted correctly. Make sure each line has a tab between the name and the date."
                 )
             name, bday = line.strip().split("\t")
-            day, month, year = bday.split("-")
+            year, month, day = map(int, bday.split("-"))
             if year < 1900 or year > 2100:
                 raise FormatError(
-                    "Invalid year in bday file. Make sure the year is between 1900 and 2100. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid year in bday file. Make sure the year ({year}) is between 1900 and 2100. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             if month < 1 or month > 12:
                 raise FormatError(
-                    "Invalid month in bday file. Make sure the month is between 1 and 12. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid month in bday file. Make sure the month ({month}) is between 1 and 12. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             if day < 1 or day > 31:
                 raise FormatError(
-                    "Invalid day in bday file. Make sure the day is between 1 and 31. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid day in bday file. Make sure the day({day}) is between 1 and 31. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             names_and_bdays.append({"name": name, "bday": bday})
 
