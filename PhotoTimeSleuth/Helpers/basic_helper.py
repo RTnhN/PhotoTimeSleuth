c7e4a1ec993241f94f411640ef76cc6715b66437
Zach Strout <43109787+RTnhN@users.noreply.github.com>
2025-06-03 22:46:30 -0500

    Fix so that right ip is shown

--- PhotoTimeSleuth/Helpers/basic_helper.py
+++ PhotoTimeSleuth/Helpers/basic_helper.py
@@ -4,6 +4,10 @@ import psutil
 def get_local_ip():
     for _, addrs in psutil.net_if_addrs().items():
         for addr in addrs:
-            if addr.family == 2 and addr.address.startswith("192.168."):
-                return addr.address  # Returns the first 192.168.x.x address found
+            if addr.family == 2 and addr.address.startswith("192.168.1."):
+                return addr.address  # Returns the first 192.168.1.x address found
     return None
+
+
+if __name__ == "__main__":
+    print(get_local_ip())
