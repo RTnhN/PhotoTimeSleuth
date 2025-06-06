acc2f23b5e2f03c817ea8a11132886c04b6bac0f
Zach Strout <43109787+RTnhN@users.noreply.github.com>
2025-06-05 22:51:18 -0500

    fix: add missing encoding and long lines

--- PhotoTimeSleuth/Helpers/file_helper.py
+++ PhotoTimeSleuth/Helpers/file_helper.py
@@ -11,27 +11,34 @@ def load_names_and_bdays(bday_file):
         return []
 
     names_and_bdays = []
-    with open(bday_file, "r") as f:
+    with open(bday_file, "r", encoding="utf-8") as f:
         for line in f:
             if line.startswith("#") or len(line.strip()) == 0:
                 continue
             if "\t" not in line:
                 raise FormatError(
-                    "It looks like the bday file is not formatted correctly. Make sure each line has a tab between the name and the date."
+                    "It looks like the bday file is not formatted correctly. \
+                     Make sure each line has a tab between the name and the date."
                 )
             name, bday = line.strip().split("\t")
             year, month, day = map(int, bday.split("-"))
             if year < 1900 or year > 2100:
                 raise FormatError(
-                    f"Invalid year in bday file. Make sure the year ({year}) is between 1900 and 2100. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid year in bday file. \
+                      Make sure the year ({year}) is between 1900 and 2100. \
+                      Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             if month < 1 or month > 12:
                 raise FormatError(
-                    f"Invalid month in bday file. Make sure the month ({month}) is between 1 and 12. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid month in bday file. \
+                      Make sure the month ({month}) is between 1 and 12. \
+                      Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             if day < 1 or day > 31:
                 raise FormatError(
-                    f"Invalid day in bday file. Make sure the day({day}) is between 1 and 31. Also make sure that the order is correct (e.g. YYYY-MM-DD)."
+                    f"Invalid day in bday file. \
+                      Make sure the day({day}) is between 1 and 31. \
+                      Also make sure that the order is correct (e.g. YYYY-MM-DD)."
                 )
             names_and_bdays.append({"name": name, "bday": bday})
 
